b373c45a8dcf1dacdfe6891b7e3cb0b2214fcd16
kevinvoli <gbamblekevin@gmail.com>
2022-06-08 18:59:20 +0000

    petite pause pour l'apero

--- controllers/inscriptionController.js
+++ controllers/inscriptionController.js
@@ -4,6 +4,9 @@ const InscriptionCOntroller= class {
     static getInscription= async(req=request, res = response, next)=>{
         res.render("inscription")
     }
+    static setInscription= async(req=request,res= response,next)=>{
+        
+    }
 }
 
 module.exports = InscriptionCOntroller
--- models/index.js
+++ models/index.js
@@ -1,11 +1,34 @@
-let users = require('../schema/users').SchemaUsers
-let message = require('../schema/users').modelsMessage
-let reponse = require('../schema/users').ModelReponse
+const  SchemaUsers= new mongoose.Schema({
+    nom: String,
+    email:String,
+    password:String,
+    image: String,
+    date:{type:Date, default:Date.now},
+    reponse:[{ type:mongoose.Schema.ObjectId, ref:'reponse-ref' } ],
+    message:[{ type:mongoose.Schema.ObjectId, ref:'message-ref' }]
+  
+  })
+  
+const  modelsMessage= new mongoose.Schema({
+      message:{type: String, default:''},
+      Date: {type:Date, default:Date.now},
+      reponse:[{ type:mongoose.Schema.ObjectId, ref:'reponse-ref' }],
+      Users: { type:mongoose.Schema.ObjectId, ref:"User-ref", childPath:"message" }
+  })
+  
+  const  ModelReponse= new mongoose.Schema({
+    reponse:{type: String, default:''},
+    Date: {type:Date, default:Date.now},
+    Users: { type:mongoose.Schema.ObjectId, ref:"User-ref", childPath:"reponse" },
+    Messages: { type:mongoose.Schema.ObjectId, ref:"message-ref", childPath:"reponse" }
+  
+  })
 
-exports.User= mongoose.model('User-ref',users) 
-message.plugin(relationship,{relationshipPathName:'Users' })
-exports.Message=mongoose.model('message-ref',message)
 
-reponse.plugin(relationship,{relationshipPathName:['Users','Messages']})
-exports.Reponse=mongoose.model('reponse-ref',reponse)
+exports.User= mongoose.model('User-ref',SchemaUsers) 
+modelsMessage.plugin(relationship,{relationshipPathName:'Users' })
+exports.Message=mongoose.model('message-ref',modelsMessage)
+
+ModelReponse.plugin(relationship,{relationshipPathName:['Users','Messages']})
+exports.Reponse=mongoose.model('reponse-ref',ModelReponse)
 
